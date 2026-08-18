04c634695e0f87de6773695b238ee077dad5ff98
Julia Rodrigues <juliarodrigues1976@gmail.com>
2026-08-18 18:45:12 -0300

    fix: formatação

--- main.java
+++ main.java
@@ -1,6 +1,62 @@
 import java.util.Scanner;
 
-public class main {
+public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
+    Jogo[] jogos = new Jogo[16]; 
+        
+
+    for (int i = 1; i <= 15; i++) {
+        jogos[i] = new Jogo();
+    }
+
+        
+    char[] letras = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'};
+
+    for (int i = 1; i <= 8; i++) {
+        jogos[i].time1 = letras[(i - 1) * 2];
+        jogos[i].time2 = letras[(i - 1) * 2 + 1];
+    }
+
+
+    int[][] dependencias = {
+        {1,2}, {3,4}, {5,6}, {7,8},
+        {9,10}, {11,12},
+        {13,14},
+        {15}
+    };
+
+        
+    for (int i = 9; i <= 15; i++) {
+        int[] dep = dependencias[i - 9];
+        jogos[i].time1 = jogos[dep[0]].vencedor;
+        jogos[i].time2 = jogos[dep[1]].vencedor;
+    }
+
+
+    for (int i = 1; i <= 15; i++) {
+        if (i >= 9) {
+            int[] dep = dependencias[i - 9];
+            jogos[i].time1 = jogos[dep[0]].vencedor;
+            jogos[i].time2 = jogos[dep[1]].vencedor;
+    }
+
+    jogos[i].golsTime1 = scanner.nextInt();
+    jogos[i].golsTime2 = scanner.nextInt();
+
+    jogos[i].vencedor = (jogos[i].golsTime1 > jogos[i].golsTime2)
+            ? jogos[i].time1
+            : jogos[i].time2;
+    }
+
+    System.out.println("O time vencedor do jogo 15 é: " + jogos[15].vencedor);
+    scanner.close();
+}
+
+static class Jogo {
+    char time1;
+    char time2;
+    int golsTime1;
+    int golsTime2;
+    char vencedor;
 }
